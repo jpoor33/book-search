d5d495350d0cf1f0e20981a9c32a425be0965304
jpoor33 <74677747+jpoor33@users.noreply.github.com>
2025-02-21 14:59:15 -0500

    changing again

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Load environment variables
 
-const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
+const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/googlebooks';
 
 mongoose
   .connect(mongoURI)
